4ccf2f9e77b7cb2229520335d3609163b712c93e
jdharmawan <d.harmawan.joshua@gmail.com>
2023-02-21 20:44:57 +0800

    Week 2 Module and Solution
    Most practices and assignment for Week 2 Materials. Some are in the Unreal project itself but my PATH was too long to include.

--- FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cpp
+++ FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cpp
@@ -1,5 +1,7 @@
 // Copyright J. Dharmawan. All Rights Reserved.
 
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 
 using namespace std;
@@ -10,5 +12,47 @@ using namespace std;
 */
 int main()
 {
-    cout << "Hello World!\n";
+    //cout << "Hello World!\n";
+    float Angle;
+
+    std::cout << "Enter an angle: ";
+    std::cin >> Angle;
+
+    Angle = Angle * M_PI / 180;
+
+    float Cosine = cosf(Angle);
+    float Sine = sinf(Angle);
+
+    std::cout << "Cosine of Angle: " << Cosine << "\n";
+    std::cout << "Sine of Angle: " << Sine << "\n";
+}
+
+void Exercise4()
+{
+    int FirstLocation;
+    int SecondLocation;
+
+    std::cout << "Enter the first altitutde: ";
+    std::cin >> FirstLocation;
+    std::cout << "Enter the second altittude: ";
+    std::cin >> SecondLocation;
+
+    std::cout << "The altitude change is: " << SecondLocation - FirstLocation;
+}
+
+void Exercise6()
+{
+    float Angle;
+
+    std::cout << "Enter an angle: ";
+    std::cin >> Angle;
+
+    Angle = Angle * M_PI / 180;
+
+    float Cosine = cosf(Angle);
+    float Sine = sinf(Angle);
+
+    std::cout << "Cosine of Angle: " << Cosine << "\n";
+    std::cout << "Sine of Angle: " << Sine << "\n";
+    
 }
